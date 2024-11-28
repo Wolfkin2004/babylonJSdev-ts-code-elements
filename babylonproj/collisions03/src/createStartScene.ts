93e0c7f7bad38da5cb76c41802e955c3cb6736bf
Cole <c.connor2004@gmail.com>
2024-11-28 14:07:35 +0000

    nearly done

--- babylonproj/collisions03/src/createStartScene.ts
+++ babylonproj/collisions03/src/createStartScene.ts
@@ -1,7 +1,3 @@
-// import "@babylonjs/core/Debug/debugLayer";
-// import "@babylonjs/inspector";
-// import "@babylonjs/core/Debug/debugLayer";
-// import "@babylonjs/inspector";
 import { SceneData } from "./interfaces";
 
 import {
