6c7105eb335845856020f7d27482f6cb42ea6d9b
Cole <c.connor2004@gmail.com>
2024-12-15 03:36:12 +0000

    done basicallly

--- babylonproj/swapper/src/scene2/createStartScene.ts
+++ babylonproj/swapper/src/scene2/createStartScene.ts
@@ -1,98 +1,146 @@
 // import "@babylonjs/core/Debug/debugLayer";
 // import "@babylonjs/inspector";
 import {
-    Scene,
-    ArcRotateCamera,
-    Vector3,
-    HemisphericLight,
-    MeshBuilder,
-    Mesh,
-    Light,
-    Camera,
-    Engine,
-    StandardMaterial,
-    Texture,
-    Color3
-  } from "@babylonjs/core";
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  MeshBuilder,
+  Mesh,
+  Light,
+  Camera,
+  Engine,
+  StandardMaterial,
+  Texture,
+  Color3,
+  PointLight,
+  DirectionalLight,
+  SpotLight,
   
-  
-  function createCylinder(scene: Scene) {
-    let cylinder = MeshBuilder.CreateCylinder(
-      "cylinder",
-      { height: 1, diameter: 0.7 },
-      scene
-    );
-    cylinder.position.x = 1;
-    cylinder.position.y = 1;
-    cylinder.position.z = 1;
-  
-    var texture = new StandardMaterial("reflective", scene);
-    texture.ambientTexture = new Texture("./assets/reflectivity.png", scene);
-    texture.diffuseColor = new Color3(1, 1, 1);
-    cylinder.material = texture;
-    return cylinder;
+} from "@babylonjs/core";
+
+
+function createCylinder(scene: Scene) {
+  let cylinder = MeshBuilder.CreateCylinder(
+    "cylinder",
+    { height: 1, diameter: 0.7 },
+    scene
+  );
+  cylinder.position.x = 1;
+  cylinder.position.y = 1;
+  cylinder.position.z = 1;
+
+  var texture = new StandardMaterial("reflective", scene);
+  texture.ambientTexture = new Texture("./assets/textures/reflectivity.png", scene);
+  texture.diffuseColor = new Color3(1, 1, 1);
+  cylinder.material = texture;
+  return cylinder;
+}
+
+function createPointLight(scene: Scene ){
+const light = new PointLight("light", new Vector3(-1, 1, 0),scene);
+light.position = new Vector3(5, 20, 10);
+light.intensity = 0.3;
+light.diffuse = new Color3(0.5, 1, 1);
+light.specular = new Color3(0.8, 1, 1);
+return light;
+}
+
+function createDirectionalLight(scene: Scene ){
+const light = new DirectionalLight("light", new Vector3(0.2, -1, 0.2),scene);
+light.position = new Vector3(20, 40, 20);
+light.intensity = 0.7;
+light.diffuse = new Color3(1, 0, 0);
+light.specular = new Color3(0, 1, 0);
+return light;
+}
+
+function createSpotLight(scene: Scene ){
+const light = new SpotLight("light", new Vector3(1, 5, -3), 
+ new Vector3(0, -1, 0), Math.PI / 3, 20, scene);
+light.intensity = 0.5;
+light.diffuse = new Color3(1, 0, 0);
+light.specular = new Color3(0, 1, 0);
+return light;
+}
+
+function createHemisphericLight(scene: Scene ){
+const light:HemisphericLight = new HemisphericLight("light", new Vector3(1, 10, 0),scene);
+light.intensity = 0.3;
+light.diffuse = new Color3(1, 0, 0);
+light.specular = new Color3(0, 1, 0);
+light.groundColor = new Color3(0, 1, 0);
+return light;
+}
+
+
+function createLight(scene: Scene) {
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  light.intensity = 0.7;
+  return light;
+}
+
+function createSphere(scene: Scene) {
+  let sphere = MeshBuilder.CreateSphere(
+    "sphere",
+    { diameter: 2, segments: 32 },
+    scene,
+  );
+  sphere.position.y = 1;
+  return sphere;
+}
+
+function createGround(scene: Scene) {
+  let ground = MeshBuilder.CreateGround(
+    "ground",
+    { width: 6, height: 6 },
+    scene,
+  );
+  return ground;
+}
+
+function createArcRotateCamera(scene: Scene) {
+  let camAlpha = -Math.PI / 2,
+    camBeta = Math.PI / 2.5,
+    camDist = 10,
+    camTarget = new Vector3(0, 0, 0);
+  let camera = new ArcRotateCamera(
+    "camera1",
+    camAlpha,
+    camBeta,
+    camDist,
+    camTarget,
+    scene,
+  );
+  camera.attachControl(true);
+  return camera;
+}
+
+export default function createStartScene(engine: Engine) {
+  interface SceneData {
+    scene: Scene;
+    cylinder?: Mesh;
+    light?: Light;
+    sphere?: Mesh;
+    ground?: Mesh;
+    camera?: Camera;
+    lightBulb?: PointLight,
+    lightDirectional?: DirectionalLight,
+    lightSpot?: SpotLight,
+    lightHemispheric?: HemisphericLight,
+ 
   }
 
-  
-  function createLight(scene: Scene) {
-    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-    return light;
-  }
-  
-  function createSphere(scene: Scene) {
-    let sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 2, segments: 32 },
-      scene,
-    );
-    sphere.position.y = 1;
-    return sphere;
-  }
-  
-  function createGround(scene: Scene) {
-    let ground = MeshBuilder.CreateGround(
-      "ground",
-      { width: 6, height: 6 },
-      scene,
-    );
-    return ground;
-  }
-  
-  function createArcRotateCamera(scene: Scene) {
-    let camAlpha = -Math.PI / 2,
-      camBeta = Math.PI / 2.5,
-      camDist = 10,
-      camTarget = new Vector3(0, 0, 0);
-    let camera = new ArcRotateCamera(
-      "camera1",
-      camAlpha,
-      camBeta,
-      camDist,
-      camTarget,
-      scene,
-    );
-    camera.attachControl(true);
-    return camera;
-  }
-  
-  export default function createStartScene(engine: Engine) {
-    interface SceneData {
-      scene: Scene;
-      cylinder?: Mesh;
-      light?: Light;
-      sphere?: Mesh;
-      ground?: Mesh;
-      camera?: Camera;
-    }
-  
-    let that: SceneData = { scene: new Scene(engine) };
-    // that.scene.debugLayer.show();
-  
-    that.cylinder = createCylinder(that.scene);
-    that.light = createLight(that.scene);
-    that.sphere = createSphere(that.scene);
-    that.ground = createGround(that.scene);
-    that.camera = createArcRotateCamera(that.scene);
-    return that;
-  }
+  let that: SceneData = { scene: new Scene(engine) };
+  // that.scene.debugLayer.show();
+
+  that.cylinder = createCylinder(that.scene);
+  that.light = createLight(that.scene);
+  that.sphere = createSphere(that.scene);
+  that.ground = createGround(that.scene);
+  that.camera = createArcRotateCamera(that.scene);
+  that.lightBulb = createPointLight(that.scene);
+  that.lightDirectional = createDirectionalLight(that.scene);
+  that.lightHemispheric = createHemisphericLight(that.scene);
+  return that;
+}
--- dev.md
+++ dev.md
@@ -1,40 +1,85 @@
-# Coding notes
-just setting up
+# Markdown for Element 5 GUI
 
-# big
-## sub
+
+# GUI
+## Imports
 
 ``` 
-big
+import setSceneIndex from "./../index"
 
+import {
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    HemisphericLight,
+    MeshBuilder,
+    Mesh,
+    Light,
+    Camera,
+    Engine,
+    StandardMaterial,
+    Texture,
+    Color3,
+    CubeTexture,
+    Sound
+  } from "@babylonjs/core";
+  import * as GUI from "@babylonjs/gui";
+```
+* This starts of with importing all data that we need for babylon js and well as the GUI.
 
-    runScene.player.then((result) => {
-      let characterMoving: Boolean = false;
-      let character: AbstractMesh = result!.meshes[0];
-      if (keyDownMap["w"] || keyDownMap["ArrowUp"]) {
-        character.position.x -= 0.1;
-        character.rotation.y = (3 * Math.PI) / 2;
-        characterMoving = true;
-      }
-      if (keyDownMap["a"] || keyDownMap["ArrowLeft"]) {
-        character.position.z -= 0.1;
-        character.rotation.y = (2 * Math.PI) / 2;
-        characterMoving = true;
-      }
-      if (keyDownMap["s"] || keyDownMap["ArrowDown"]) {
-        character.position.x += 0.1;
-        character.rotation.y = (1 * Math.PI) / 2;
-        characterMoving = true;
-      }
-      if (keyDownMap["d"] || keyDownMap["ArrowRight"]) {
-        character.position.z += 0.1;
-        character.rotation.y = (0 * Math.PI) / 2;
-        characterMoving = true;
-      }
- box 
+```
+function createText(scene: Scene, theText: string, x: string, y: string, s: string, c: string, advtex) {
+    let text = new GUI.TextBlock();
+    text.text = theText;
+    text.color = c;
+    text.fontSize = s;
+    text.fontWeight = "bold"; //can add parameter for this if you wish
+    text.left = x;
+    text.top = y;
+    advtex.addControl(text);
+    return text;
+  }
+```
+* This function creates text within the GUI which we can edit everything about the text from the size to the font.
+
+```
+ function createSceneButton(scene: Scene, name: string, note: string, index: number, x: string, y: string, advtex) {
+    let button = GUI.Button.CreateSimpleButton(name, note);
+        button.left = x;
+        button.top = y;
+        button.width = "80px";
+        button.height = "30px";
+        button.color = "white";
+        button.cornerRadius = 20;
+        button.background = "purple";
+
+        /*
+        const buttonClick = new Sound("MenuClickSFX", "./audio/menu-click.wav", scene, null, {
+          loop: false,
+          autoplay: false,
+        });
+        */
+
+        button.onPointerUpObservable.add(function() {
+            console.log("THE BUTTON HAS BEEN CLICKED");
+           // buttonClick.play();
+            setSceneIndex(index -1);
+        });
+        advtex.addControl(button);
+        return button;
+ }
+```
+* This function creates the scene button which plays a sound when clicked as well as displays a message which clicked.
+
+```
+  export default function menuScene(engine: Engine) {
+    interface SceneData {
+      scene: Scene;
+      advancedTexture: GUI.AdvancedDynamicTexture;
+      button1: GUI.Button;
+      button2: GUI.Button;
+      camera: Camera;
+    }
 ```
 
-* poit
-**bold**
-***itallic***
-![markdown](images/md.png)
+* This is the main rendering section of the scene.
